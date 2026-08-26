d854cac2530073d691b3ff5041bb080f481575a2
piotrstani <piotr1670@pm.me>
2026-08-26 19:17:42 +0200

    pobieranie info

--- main.py
+++ main.py
@@ -10,3 +10,25 @@ r = requests.get(url, headers=headers)
 print(f"Kod stanu: {r.status_code}")
 response_dict=r.json()
 print(response_dict.keys())
+
+
+print(f"Liczba repos: {response_dict['total_count']}")
+
+repo_dicts=response_dict['items']
+
+print(f"Liczba zwróconychy repos: {len(repo_dicts)}")
+
+repo_dict=repo_dicts[0]
+print(f"\nKlucze: {len(repo_dict)}")
+
+#for key in sorted(repo_dict.keys()):
+#    print(f"{key}")
+i=0
+for repo in repo_dicts:
+    i += 1
+    print(f"\n{i}")
+    print(f"Nazwa: {repo_dict['name']}")
+    print(f"Właściciel: {repo_dict['owner']['login']}")
+    print(f"Gwiazdki: {repo_dict['stargazers_count']}")
+    print(f"Repozytorium: {repo_dict['html_url']}")
+    print(f"Opis: {repo_dict['description']}")
